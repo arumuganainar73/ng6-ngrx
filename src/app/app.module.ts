3344f6c956e48bab52195f4981e126a0341d5009
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2018-08-04 14:48:45 +0530

    added angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatToolbarModule, MatButtonModule, MatSidenavModule,
+  MatIconModule, MatListModule, MatCardModule, MatInput, MatInputModule, MatTableModule
+} from '@angular/material';
+
+
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { FakeBackendService } from 'src/app/fake-backend/fake-backend.service';
+
+
+const appRoutes: Routes = [
+/*   { path: 'first-page', component: FirstPageComponent }, */
+  { path: '', redirectTo: 'first-page', pathMatch: 'full' },
+  { path: '**', redirectTo: 'first-page' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    InMemoryWebApiModule.forRoot(FakeBackendService),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes , {useHash: true}),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatInputModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
